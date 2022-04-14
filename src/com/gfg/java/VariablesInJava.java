4fde0368c6e9d121a3000cc7b4a1217b845be2f0
Sainathan <sainathaniyer@gmail.com>
2022-04-14 16:56:53 +0530

    Variables and Data Types

---
+++ src/com/gfg/java/VariablesInJava.java
@@ -0,0 +1,18 @@
+package com.gfg.java;
+
+public class VariablesInJava {
+	public static void main(String args[]) {
+		int age = 23;
+		String name = "Sainath";
+		float weight = 75.4F;
+		System.out.println(age);
+		System.out.println(name);
+		System.out.println(weight);
+		boolean isValid = true;
+        byte marks = 90;
+        float pi = 3.14f;
+        float div = 15.0f/4.0f;
+        long views = 10000000000l;
+        char gender = 'M';
+	}
+}
