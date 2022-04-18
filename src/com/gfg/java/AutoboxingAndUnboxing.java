f34acef3b54dac5ddebfd5bd2d77ca3d2a7e0a0c
Sainathan <sainathaniyer@gmail.com>
2022-04-19 00:21:20 +0530

    Autoboxing and Unboxing

---
+++ src/com/gfg/java/AutoboxingAndUnboxing.java
@@ -0,0 +1,13 @@
+package com.gfg.java;
+
+public class AutoboxingAndUnboxing {
+	public static void main(String args[]) {
+		int x1 = 10;
+		Integer x2 = x1;
+		int x3 = x2;
+		System.out.println(x1);
+		System.out.println(x2);
+		System.out.println(x3);
+	}
+
+}
