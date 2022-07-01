93d9419e1adf58d2d872e4cf8b9f826acb3d3d47
Sainathan Iyer <sainathaniyer@gmail.com>
2022-07-01 07:53:04 +0530

    updated test file

--- src/com/gfg/java/Test.java
+++ src/com/gfg/java/Test.java
@@ -2,7 +2,7 @@ package com.gfg.java;
 
 public class Test{
 	public static void main(String args[]) {
-		int x = 112345;
-		System.out.println(~x);
+		int x = -1;
+		System.out.println(x<<1);
 	}
 }
