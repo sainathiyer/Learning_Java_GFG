e4e81327d25e5129de9788654dc92114185ea22a
Sainathan Iyer <sainathaniyer@gmail.com>
2022-08-19 10:08:49 +0530

    updated code

---
+++ src/com/gfg/java/CountDistinctElements.java
@@ -0,0 +1,21 @@
+package com.gfg.java;
+
+public class CountDistinctElements {
+	public static void main(String args[]) {
+		int arr[] = {10,20,30,1,10,20};
+		int count=0;
+		boolean isDistinct = true;
+		for(int i=0;i<arr.length;i++) {
+			for(int j = i-1;j>=0;j--) {
+				if(arr[i]==arr[j]) {
+					isDistinct = false;
+					break;
+				}
+			}
+			if(isDistinct == true) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
