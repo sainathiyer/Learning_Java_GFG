1ac1a7cb8744b65e6bd663345b1b2edd494200e6
Sainathan Iyer <sainathaniyer@gmail.com>
2022-08-27 20:34:43 +0530

    updated code

---
+++ src/com/gfg/java/StringsInJava.java
@@ -0,0 +1,11 @@
+package com.gfg.java;
+
+public class StringsInJava {
+	public static void main(String args[]) {
+		String str = "geeks";
+		System.out.println(str.length());
+		System.out.println(str.charAt(3));
+		System.out.println(str.substring(2));
+		System.out.println(str.substring(2,5));
+	}
+}
