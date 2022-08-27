ad0c55f1d79f82fc70954c8231c9421d60bbf9ee
Sainathan Iyer <sainathaniyer@gmail.com>
2022-08-27 20:30:17 +0530

    Advanced Arrays

--- src/com/gfg/java/JaggedArray.java
+++ src/com/gfg/java/JaggedArray.java
@@ -1,11 +1,13 @@
 package com.gfg.java;
 
-public class MultidimensionalArrays {
+public class JaggedArray {
 	public static void main(String args[]) {
-		int arr[][] = {{1,2,3,8,9},
-					   {4,5,6}};
+		int m = 3;
+		int arr[][] = new int[m][];
 		for(int i=0;i<arr.length;i++) {
+			arr[i] = new int[i+1];
 			for(int j=0;j<arr[i].length;j++) {
+				arr[i][j] = i;
 				System.out.print(arr[i][j]+" ");
 			}
 			System.out.println();
