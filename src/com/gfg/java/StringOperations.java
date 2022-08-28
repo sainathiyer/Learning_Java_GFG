ddbc5c44e12301fda14e8daa0eeaeb3563f55640
Sainathan Iyer <sainathaniyer@gmail.com>
2022-08-28 16:25:39 +0530

    String Operations

---
+++ src/com/gfg/java/StringOperations.java
@@ -0,0 +1,11 @@
+package com.gfg.java;
+
+public class StringOperations {
+	public static void main(String args[]) {
+		String s1 = "HELLO";
+		String s2 = "hello";
+		System.out.println(s1.equalsIgnoreCase(s2));
+		String str = "sainathan";
+		System.out.println(str.toUpperCase());
+	}
+}
