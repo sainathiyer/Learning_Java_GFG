277a37c823b1afbf7cff3a3baa971920c4c9f502
Sainathan Iyer <sainathaniyer@gmail.com>
2022-08-29 00:28:19 +0530

    updated codes

--- src/com/gfg/java/StringBuilderBufferMethods.java
+++ src/com/gfg/java/StringBuilderBufferMethods.java
@@ -1,6 +1,6 @@
 package com.gfg.java;
 
-public class StringBuilderVsString {
+public class StringBuilderBufferMethods {
 	public static void main(String args[]) {
 		String s1 = "geeks";
 		String s2 = s1;
@@ -18,6 +18,18 @@ public class StringBuilderVsString {
 		}else {
 			System.out.println("Not Same");
 		}
+		sb1.insert(0, "false");
+		System.out.println(sb1);
+		sb1.setCharAt(0,'g');
+		System.out.println(sb1);
+		sb1.reverse();
+		System.out.println(sb1);
+		sb1.deleteCharAt(0);
+		System.out.println(sb1);
+		sb1.delete(0, 4);
+		System.out.println(sb1);
+		System.out.println(sb1.capacity());
+		sb1.replace(0, 3, "sai");
+		System.out.println(sb1);
 	}
-
 }
