95c92682055eb86809748d012cd4276cd4aa32c1
Sainathan Iyer <sainathaniyer@gmail.com>
2022-09-03 15:22:34 +0530

    String Pattern Search

--- src/com/gfg/java/StringPatternSearch.java
+++ src/com/gfg/java/StringPatternSearch.java
@@ -1,6 +1,8 @@
 package com.gfg.java;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class StringPatternSearch {
 	public static void main(String args[]) {
@@ -8,10 +10,12 @@ public class StringPatternSearch {
 		String txt = sc.nextLine();
 		String pat = sc.next();
 		sc.close();
-		int pos = txt.indexOf(pat);
-		while(pos >= 0) {
-			System.out.print(pos+" ");
-			pos = txt.indexOf(pat,pos+1);
+		Set<Integer> sts = new HashSet<>();
+		for(int i=0;i<txt.length();i++) {
+			if(txt.indexOf(pat,i)>=0) {
+			sts.add(txt.indexOf(pat,i));
+			}
 		}
+		System.out.println(sts);
 	}
 }
