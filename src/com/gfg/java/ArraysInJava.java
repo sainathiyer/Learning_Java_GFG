1dae341969869665c45378b52a2510ba2902c6f5
sainathiyer <sainathaniyer@gmail.com>
2023-09-17 10:46:08 +0530

    Initial Commit

--- src/com/gfg/java/ArraysInJava.java
+++ src/com/gfg/java/ArraysInJava.java
@@ -4,13 +4,13 @@ public class ArraysInJava {
 	public static void main(String args[]) {
 		int []a = {8,2,6,4,3};
 		int sum=0;
-		int sqSum=0;
+		int squareSum=0;
 		//Enhanced For Loop
 		for(int i:a) {
 			sum += i;
-			sqSum += i*i;
+			squareSum += i*i;
 		}
 		System.out.println(sum);
-		System.out.println(sqSum);
+		System.out.println(squareSum);
 	}
 }
